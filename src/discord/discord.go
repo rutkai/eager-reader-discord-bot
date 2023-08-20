669b91992ebb9c682e278694d2fe1c26dfc562f8
András Rutkai <riskawarrior@live.com>
2023-08-20 10:55:33 +0200

    Add blacklist

--- src/discord/discord.go
+++ src/discord/discord.go
@@ -6,7 +6,10 @@ import (
 	"github.com/rs/zerolog/log"
 	"os"
 	"regexp"
+	"strings"
 )
+
+var blacklist = []string{"youtube.com"}
 
 func StartBot() {
 	discord, err := discordgo.New("Bot " + getBotToken())
@@ -50,6 +53,11 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 	log.Debug().Str("Url", url).Msg("URL found in message")
 
+	if isBlacklisted(url) {
+		log.Debug().Str("Url", url).Msg("URL is blacklisted")
+		return
+	}
+
 	summary, err := ai.GetSummary(url, "")
 	if err != nil {
 		_, err = s.ChannelMessageSend(m.ChannelID, "Something went wrong with generating the summary.")
@@ -66,4 +74,13 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 func parseUrl(url string) string {
 	re := regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
 	return re.FindString(url)
+}
+
+func isBlacklisted(url string) bool {
+	for _, blacklistedUrl := range blacklist {
+		if strings.Contains(url, blacklistedUrl) {
+			return true
+		}
+	}
+	return false
 }
